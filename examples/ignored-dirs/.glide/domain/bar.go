9c1850e762e9bcc93cafd0f49def00c924fe6727
Robert Laszczak <eventched@gmail.com>
2017-06-07 18:44:40 +0200

    added missing docs for examples

--- examples/ignored-dirs/.glide/domain/bar.go
+++ examples/ignored-dirs/.glide/domain/bar.go
@@ -2,6 +2,7 @@ package domain
 
 import "github.com/roblaszczak/go-cleanarch/examples/ignored-dirs/app"
 
+// Bar is not validated, because all dirs starting with "." are ignored.
 type Bar struct {
 	foo app.Foo
 }
--- examples/ignored-dirs/app/foo.go
+++ examples/ignored-dirs/app/foo.go
@@ -1,3 +1,4 @@
 package app
 
+// Foo shouldn't be imported from app layer to domain layer, but all dirs with imports Foo are ignored.
 type Foo int
